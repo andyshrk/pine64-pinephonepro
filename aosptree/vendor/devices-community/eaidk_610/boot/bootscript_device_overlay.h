da9561d6fe361edca22f3e2907d3a1ce5ec5d0db
Andy Yan <andyshrk@163.com>
2023-05-04 14:45:08 +0800

    Basic support for eaidk
    Signed-off-by: Andy Yan <andyshrk@163.com>

---
+++ aosptree/vendor/devices-community/eaidk_610/boot/bootscript_device_overlay.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "bootscript.h"
+
+/* GPIOs: don't ask, just do :)
+ * GPIO4 D2 - Red LED
+ * Set: $ gpio set E26
+ * GPIO4 D5 - Green LED
+ * Set: $ gpio set E29
+ * GPIO4 D6 - Blue LED
+ * Set: $ gpio set E30
+ */
+
